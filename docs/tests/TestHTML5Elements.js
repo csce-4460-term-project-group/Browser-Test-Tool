25c0161d594c21efc226033b092fe1679e4fcd66
shinylys <lysettebarrera@gmail.com>
2019-03-03 20:03:03 -0600

    added blank array to TestHTML5GlobalAttributes.js and TestHTML5Elements.js

--- docs/tests/TestHTML5Elements.js
+++ docs/tests/TestHTML5Elements.js
@@ -1,6 +1,8 @@
 function TestHTML5Elements() {
 	var test = new BrowserTest();
-	test.title = "Elements";
+    test.title = "Elements";
+    test.testDescriptions = [];
+    test.results = [];
 	var arrayOfElements = ["html", "head", "title", "base", "link", "meta", "style", "body", "article", "section", "nav", "aside", "h1", "h2", "h3", "h4", "h5", "h6", "header", "footer", "p", "address", "hr", "pre", "blockquote", "ol", "ul", "li", "dl", "dt", "dd", "figure", "figcaption", "main", "div", "a", "em", "strong", "small", "s", "cite", "q", "dfn", "abbr", "ruby", "rb", "rt", "rtc", "rp", "data", "time", "code", "var", "samp", "kbd", "sub", "sup", "i", "b", "u", "mark", "bdi", "bdo", "span", "br", "wbr", "ins", "del", "picture", "source", "img", "iframe", "embed", "object", "param", "video", "audio", "track", "map", "area", "table", "caption", "colgroup", "col", "tbody", "thead", "tfoot", "tr", "td", "th", "form", "label", "input", "button", "select", "datalist", "optgroup", "option", "textarea", "output", "progress", "meter", "fieldset", "legend", "details", "summary", "dialog", "script", "noscript", "template", "canvas"];
 	for (var i = 0; i < arrayOfElements.length; i++) {
 		test.testDescriptions.push("<" + arrayOfElements[i] + ">");
--- docs/tests/TestHTML5GlobalAttributes.js
+++ docs/tests/TestHTML5GlobalAttributes.js
@@ -1,12 +1,13 @@
 function TestHTML5GlobalAttributes() {
     var test = new BrowserTest();
     test.title = "Attributes";
+    test.testDescriptions = [];
+    test.results = [];
     var arrayOfAttributes = ["accessKey", "className", "contentEditable", "dir", "draggable", "hidden", "id", "lang", "spellcheck", "style", "tabIndex", "title", "translate"];
     var myElem = document.createElement("a");
 
     for (var i = 0; i < arrayOfAttributes.length; i++) {
         test.testDescriptions.push("<" + arrayOfAttributes[i] + ">");
-
         test.results.push((arrayOfAttributes[i]) in myElem);
 
     }
