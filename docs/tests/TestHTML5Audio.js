da04dd601dd72fc637796364ba5af55a272fa2cd
RichardLin1 <RichardLin@my.unt.edu>
2019-03-05 13:39:55 -0600

    Push true or false instead of maybe or probably

--- docs/tests/TestHTML5Audio.js
+++ docs/tests/TestHTML5Audio.js
@@ -5,8 +5,10 @@ function TestHTML5Audio() {
     var audioFormats = ["audio/mp3", "audio/ogg", "audio/wav"];
     var audio = document.createElement("audio");
     if (audio instanceof HTMLAudioElement)
-        for (var i = 0; i < audioFormats.length; i++)
-            test.results.push(audio.canPlayType(audioFormats[i]));
+        for (var i = 0; i < audioFormats.length; i++) {
+            var canPlay = audio.canPlayType(audioFormats[i]);
+            test.results.push(canPlay == "maybe" || canPlay == "probably");
+        }
     else
         for (var i = 0; i < audioFormats.length; i++)
             test.results.push(false);
