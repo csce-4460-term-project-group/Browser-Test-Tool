c711dca3728901b47d8c5ca2690bbb7644a1f125
RichardLin1 <RichardLin@my.unt.edu>
2019-03-10 15:56:09 -0500

    Create test for Selectors

---
+++ docs/scripts/TestCSSSelectors.js
@@ -0,0 +1,37 @@
+function TestCSSSelectors() {
+    var test = new BrowserTest();
+    test.title = "Selectors Level 3";
+    var selectors = ['*', 'E', 'E[foo]', 'E[foo="bar"]', 'E[foo~="bar"]', 'E[foo^="bar"]', 'E[foo$="bar"]', 'E[foo*="bar"]', 'E[foo|="en"]', 'E:root', 'E:nth-child(n)', 'E:nth-last-child(n)', 'E:nth-of-type(n)', 'E:nth-last-of-type(n)', 'E:first-child', 'E:last-child', 'E:first-of-type', 'E:last-of-type', 'E:only-child', 'E:only-of-type', 'E:empty', 'E:link', 'E:visited', 'E:active', 'E:hover', 'E:focus', 'E:target', 'E:lang(fr)', 'E:enabled', 'E:disabled', 'E:checked', 'E::first-line', 'E::first-letter', 'E::before', 'E::after', 'E.warning', 'E#myid', 'E:not(s)', 'E F', 'E > F', 'E + F', 'E ~ F'];
+    var element = document.createElement("a");
+    if ("matches" in element)
+        for (var i = 0; i < selectors.length; i++) {
+            test.testDescriptions.push(selectors[i]);
+            try {
+                element.matches(selectors[i]);
+                test.results.push(true);
+            } catch (e) {
+                test.results.push(false);
+            }
+        }
+    else if ("msMatchesSelector" in element)
+        for (var i = 0; i < selectors.length; i++) {
+            test.testDescriptions.push(selectors[i]);
+            try {
+                element.msMatchesSelector(selectors[i]);
+                test.results.push(true);
+            } catch (e) {
+                test.results.push(false);
+            }
+        }
+    else if ("webkitMatchesSelector" in element)
+        for (var i = 0; i < selectors.length; i++) {
+            test.testDescriptions.push(selectors[i]);
+            try {
+                element.webkitMatchesSelector(selectors[i]);
+                test.results.push(true);
+            } catch (e) {
+                test.results.push(false);
+            }
+        }
+    return test;
+}
