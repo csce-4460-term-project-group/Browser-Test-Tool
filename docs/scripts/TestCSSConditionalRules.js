d2690a9fae53029f2ecd8fba60dbd048652c18a6
RichardLin1 <RichardLin@my.unt.edu>
2019-03-13 17:22:49 -0500

    Remove tests checking for interfaces

--- docs/scripts/TestCSSConditionalRules.js
+++ docs/scripts/TestCSSConditionalRules.js
@@ -8,47 +8,5 @@ function TestCSSConditionalRules() {
     } catch (e) {
         test.results.push(false);
     }
-    test.testDescriptions.push("CSSRule");
-    try {
-        CSSRule;
-        test.results.push(true);
-    } catch (e) {
-        test.results.push(false);
-    }
-    test.testDescriptions.push("CSSGroupingRule");
-    try {
-        CSSGroupingRule;
-        test.results.push(true);
-    } catch (e) {
-        test.results.push(false);
-    }
-    test.testDescriptions.push("CSSConditionRule");
-    try {
-        CSSConditionRule;
-        test.results.push(true);
-    } catch (e) {
-        test.results.push(false);
-    }
-    test.testDescriptions.push("CSSMediaRule");
-    try {
-        CSSMediaRule;
-        test.results.push(true);
-    } catch (e) {
-        test.results.push(false);
-    }
-    test.testDescriptions.push("CSSSupportsRule");
-    try {
-        CSSSupportsRule;
-        test.results.push(true);
-    } catch (e) {
-        test.results.push(false);
-    }
-    test.testDescriptions.push("CSS");
-    try {
-        CSS;
-        test.results.push(true);
-    } catch (e) {
-        test.results.push(false);
-    }
     return test;
 }
