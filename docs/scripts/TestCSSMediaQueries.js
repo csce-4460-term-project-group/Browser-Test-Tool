ca45e98ae9552c861fd5964b35d9d8daa216817b
RichardLin1 <RichardLin@my.unt.edu>
2019-03-13 19:19:17 -0500

    Edit test descriptions

--- docs/scripts/TestCSSMediaQueries.js
+++ docs/scripts/TestCSSMediaQueries.js
@@ -1,9 +1,11 @@
 function TestCSSMediaQueries() {
     var test = new BrowserTest();
     test.title = "Media Queries";
-    test.testDescriptions = ["Media features: width", "Media features: height", "Media features: device-width", "Media features: device-height", "Media features: orientation", "Media features: aspect-ratio", "Media features: device-aspect-ratio", "Media features: color", "Media features: color-index", "Media features: monochrome", "Media features: resolution", "Media features: scan", "Media features: grid"];
+    var mediaFeatures = ["width", "height", "device-width", "device-height", "orientation", "aspect-ratio", "device-aspect-ratio", "color", "color-index", "monochrome", "resolution", "scan", "grid"];
     var mediaQueries = ["(width)", "(height)", "(device-width)", "(device-height)", "(orientation)", "(aspect-ratio)", "(device-aspect-ratio)", "(color), (color: 0)", "(color-index), (color-index: 0)", "(monochrome), (monochrome: 0)", "(resolution)", "tv and (scan), not tv and (scan)", "(grid: 1), (grid: 0)"];
-    for (var i = 0; i < mediaQueries.length; i++)
+    for (var i = 0; i < mediaQueries.length; i++) {
+        test.testDescriptions.push("'" + mediaFeatures[i] + "' media feature");
         test.results.push(Modernizr.mq(mediaQueries[i]));
+    }
     return test;
 }
