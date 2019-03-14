b53a0109cb9d65524f04c3b44560fe0a85bc8f52
RichardLin1 <RichardLin@my.unt.edu>
2019-03-13 19:21:33 -0500

    Edit test descriptions

--- docs/scripts/TestCSSMediaQueries.js
+++ docs/scripts/TestCSSMediaQueries.js
@@ -4,7 +4,7 @@ function TestCSSMediaQueries() {
     var mediaFeatures = ["width", "height", "device-width", "device-height", "orientation", "aspect-ratio", "device-aspect-ratio", "color", "color-index", "monochrome", "resolution", "scan", "grid"];
     var mediaQueries = ["(width)", "(height)", "(device-width)", "(device-height)", "(orientation)", "(aspect-ratio)", "(device-aspect-ratio)", "(color), (color: 0)", "(color-index), (color-index: 0)", "(monochrome), (monochrome: 0)", "(resolution)", "tv and (scan), not tv and (scan)", "(grid: 1), (grid: 0)"];
     for (var i = 0; i < mediaQueries.length; i++) {
-        test.testDescriptions.push("'" + mediaFeatures[i] + "' media feature");
+        test.testDescriptions.push(mediaFeatures[i]);
         test.results.push(Modernizr.mq(mediaQueries[i]));
     }
     return test;
