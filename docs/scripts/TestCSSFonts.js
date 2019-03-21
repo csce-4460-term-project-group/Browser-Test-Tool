a83df8677e7896306fb4a0d524d3b39f927d0120
Lysette <47045426+shinylys@users.noreply.github.com>
2019-03-21 13:26:28 -0500

    Add files via upload
    Basic User Interface and Fonts scripts

---
+++ docs/scripts/TestCSSFonts.js
@@ -0,0 +1,11 @@
+function TestCSSFonts() {
+    var test = new BrowserTest();
+    test.title = "Fonts";
+	var properties = ['font-family', 'font-weight', 'font-stretch', 'font-style', 'font-size', 'font-size-adjust', 'font', 'font-synthesis', 'src', 'unicode-range', 'font-feature-settings', 'font-kerning', 'font-variant-ligatures', 'font-variant-position', 'font-variant-caps', 'font-variant-numeric', 'font-variant-east-asian', 'font-variant'];
+	for (var i = 0; i < properties.length; i++){
+		test.testDescriptions.push(properties[i]);
+        test.results.push(Modernizr.testProp(properties[i]));
+
+	}
+    return test;
+}
