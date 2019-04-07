90800ed2b163eab2de3049acec9d7c06f66a9101
shinylys <lysettebarrera@gmail.com>
2019-04-07 18:57:57 -0500

    got rid of top, bottom, left, and right from list of properties ('position' covers them already)

--- docs/scripts/TestCSSVisualFormattingModel.js
+++ docs/scripts/TestCSSVisualFormattingModel.js
@@ -1,7 +1,7 @@
 function TestCSSVisualFormattingModel() {
     var test = new BrowserTest();
     test.title = "Visual Formatting Model";
-    var properties = ['display', 'position', 'top', 'right', 'bottom', 'left', 'float', 'clear', 'z-index', 'direction', 'unicode-bidi', 'width', 'min-width', 'max-width', 'height', 'min-height', 'max-height', 'line-height', 'vertical-align'];
+    var properties = ['display', 'position', 'float', 'clear', 'z-index', 'direction', 'unicode-bidi', 'width', 'min-width', 'max-width', 'height', 'min-height', 'max-height', 'line-height', 'vertical-align'];
     for (var i = 0; i < properties.length; i++) {
         test.testDescriptions.push(properties[i]);
         test.results.push(Modernizr.testProp(properties[i]));
