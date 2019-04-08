23b1e6384d74e697fa26559cd3f5d09fa0cc4952
shinylys <lysettebarrera@gmail.com>
2019-04-07 20:06:18 -0500

    added unicode-bidi, removed direction

--- docs/scripts/TestCSSVisualFormattingModel.js
+++ docs/scripts/TestCSSVisualFormattingModel.js
@@ -1,7 +1,7 @@
 function TestCSSVisualFormattingModel() {
     var test = new BrowserTest();
     test.title = "Visual Formatting Model";
-    var properties = ['display', 'top', 'left', 'bottom', 'right', 'float', 'clear', 'z-index', 'direction', 'unicode-bidi', 'width', 'min-width', 'max-width', 'height', 'min-height', 'max-height', 'line-height', 'vertical-align'];
+    var properties = ['display', 'top', 'left', 'bottom', 'right', 'float', 'clear', 'z-index', 'unicode-bidi', 'width', 'min-width', 'max-width', 'height', 'min-height', 'max-height', 'line-height', 'vertical-align'];
     for (var i = 0; i < properties.length; i++) {
         test.testDescriptions.push(properties[i]);
         test.results.push(Modernizr.testProp(properties[i]));
