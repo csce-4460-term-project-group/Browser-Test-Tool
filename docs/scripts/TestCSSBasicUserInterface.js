052b85b569a079ba3d5bc8480f24e75ae4360538
RichardLin1 <RichardLin@my.unt.edu>
2019-04-10 17:17:18 -0500

    Remove properties that require user interaction

--- docs/scripts/TestCSSBasicUserInterface.js
+++ docs/scripts/TestCSSBasicUserInterface.js
@@ -1,11 +1,11 @@
 function TestCSSBasicUserInterface() {
     var test = new BrowserTest();
     test.title = "Basic User Interface"; //Level 3
-	var properties = ['box-sizing','outline', 'outline-width', 'outline-style', 'outline-color', 'outline-offset', 'resize', 'text-overflow', 'cursor', 'caret-color'];
-	for (var i = 0; i < properties.length; i++){
-		test.testDescriptions.push(properties[i]);
+    var properties = ['box-sizing', 'outline', 'outline-width', 'outline-style', 'outline-color', 'outline-offset', 'text-overflow'];
+    for (var i = 0; i < properties.length; i++) {
+        test.testDescriptions.push(properties[i]);
         test.results.push(Modernizr.testProp(properties[i]));
 
-	}
+    }
     return test;
 }
