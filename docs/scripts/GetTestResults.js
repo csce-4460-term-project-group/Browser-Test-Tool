c51796a10d820df512f04a107a6a0ca4b75c307d
RichardLin1 <RichardLin@my.unt.edu>
2019-04-12 16:23:12 -0500

    Remove font size

--- docs/scripts/GetTestResults.js
+++ docs/scripts/GetTestResults.js
@@ -51,7 +51,6 @@ function GetTestResults(tests, testType, divId, color) {
     title.style.paddingTop = "16pt";
     title.style.paddingBottom = "16pt";
     title.style.paddingLeft = "16pt";
-    title.style.fontSize = "xx-large";
     title.style.color = "#FFFFFF";
     title.textContent = testType;
     var score = document.createElement("span");
@@ -59,7 +58,6 @@ function GetTestResults(tests, testType, divId, color) {
     score.style.paddingTop = "16pt";
     score.style.paddingBottom = "16pt";
     score.style.paddingRight = "16pt";
-    score.style.fontSize = "xx-large";
     score.style.color = "#FFFFFF";
     score.textContent = totalTestsPassed + "/" + totalTests;
     var tabContent = document.getElementById(divId);
