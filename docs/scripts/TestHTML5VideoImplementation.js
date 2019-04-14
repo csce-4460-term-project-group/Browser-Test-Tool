730fe8cd4d5147c72672c09145a9340a69b4669c
RichardLin1 <RichardLin@my.unt.edu>
2019-04-14 12:20:39 -0500

    Create implementation test for HTML 5 Video

--- docs/scripts/TestHTML5VideoImplementation.js
+++ docs/scripts/TestHTML5VideoImplementation.js
@@ -1,21 +1,22 @@
-var totalTestsHTML5Audio = 3, testsCompletedHTML5Audio = 0;
+var totalTestsHTML5Video = 5, testsCompletedHTML5Video = 0;
 
-function TestHTML5AudioImplementation() {
+function TestHTML5VideoImplementation() {
     var test = new BrowserTest();
-    test.title = "Audio";
-    var formats = ["MP3", "Ogg", "WAV"];
-    var files = ["audio.mp3", "audio.ogg", "audio.wav"];
+    test.title = "Video";
+    var formats = ["MP4 (AVC/H.264)", "MP4 (HEVC/H.265)", "Ogg (Theora)", "WebM (VP8)", "WebM (VP9)"];
+    var files = ["video-h264.mp4", "video-h265.mp4", "video-theora.ogv", "video-vp8.webm", "video-vp9.webm"];
     for (var i = 0; i < formats.length; i++) {
         test.testDescriptions.push(formats[i]);
-        var audio = new Audio("https://csce-4460-term-project-group.github.io/Browser-Test-Tool/test-audio/" + files[i]);
-        audio.volume = 0;
-        audio.addEventListener("canplaythrough", function (parameters) {
-            var promise = parameters["audio"].play();
+        var video = document.createElement("video");
+        video.src = "https://csce-4460-term-project-group.github.io/Browser-Test-Tool/test-video/" + files[i];
+        video.volume = 0;
+        video.addEventListener("canplaythrough", function (parameters) {
+            var promise = parameters["video"].play();
             promise.then(function () {
                 parameters["test"].testDescriptionsUnsorted.push(parameters["format"]);
                 parameters["test"].results.push(true);
-                testsCompletedHTML5Audio++;
-                if (testsCompletedHTML5Audio == totalTestsHTML5Audio) {
+                testsCompletedHTML5Video++;
+                if (testsCompletedHTML5Video == totalTestsHTML5Video) {
                     parameters["test"].sort();
                     document.dispatchEvent(new Event("html-5-implementation-tests-complete"));
                     if (testsCompletedHTML5Video + testsCompletedHTML5Audio == totalTestsHTML5Video + totalTestsHTML5Audio)
@@ -24,15 +25,14 @@ function TestHTML5AudioImplementation() {
             }).catch(function () {
                 parameters["test"].testDescriptionsUnsorted.push(parameters["format"]);
                 parameters["test"].results.push(false);
-                testsCompletedHTML5Audio++;
-                if (testsCompletedHTML5Audio == totalTestsHTML5Audio) {
+                testsCompletedHTML5Video++;
+                if (testsCompletedHTML5Video == totalTestsHTML5Video) {
                     parameters["test"].sort();
-                    document.dispatchEvent(new Event("html-5-implementation-tests-complete"));
                     if (testsCompletedHTML5Video + testsCompletedHTML5Audio == totalTestsHTML5Video + totalTestsHTML5Audio)
                         document.dispatchEvent(new Event("html-5-implementation-tests-complete"));
                 }
             });
-        }({ test: test, audio: audio, format: formats[i] }));
+        }({ test: test, video: video, format: formats[i] }));
     }
     return test;
 }
