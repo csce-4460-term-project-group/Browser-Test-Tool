50e9f8d8fa51f5d6e95d2834dec5e792dbd8fd2a
RichardLin1 <RichardLin@my.unt.edu>
2019-04-13 19:31:59 -0500

    Create implementation test for HTML 5 Audio

---
+++ docs/scripts/TestHTML5AudioImplementation.js
@@ -0,0 +1,30 @@
+var totalTestsHTML5Audio = 3, testsCompletedHTML5Audio = 0;
+
+function TestHTML5AudioImplementation() {
+    var test = new BrowserTest();
+    test.title = "Audio";
+    var formats = ["MP3", "Ogg", "WAV"];
+    var files = ["audio.mp3", "audio.ogg", "audio.wav"];
+    for (var i = 0; i < formats.length; i++) {
+        test.testDescriptions.push(formats[i]);
+        var audio = new Audio("https://csce-4460-term-project-group.github.io/Browser-Test-Tool/test-audio/" + files[i]);
+        audio.volume = 0;
+        audio.addEventListener("canplaythrough", function (parameters) {
+            var promise = parameters["audio"].play();
+            promise.then(function () {
+                parameters["test"].testDescriptionsUnsorted(parameters["format"]);
+                parameters["test"].results.push(true);
+                testsCompletedHTML5Audio++;
+                if (testsCompletedHTML5Audio == totalTestsHTML5Audio)
+                    parameters["test"].sort();
+            }).catch(function () {
+                parameters["test"].testDescriptionsUnsorted(parameters["format"]);
+                parameters["test"].results.push(false);
+                testsCompletedHTML5Audio++;
+                if (testsCompletedHTML5Audio == totalTestsHTML5Audio)
+                    parameters["test"].sort();
+            });
+        }({ test: test, audio: audio, format: formats[i] }));
+    }
+    return test;
+}
