b1886da8200982a74cefd09046b54b50ff27210a
RichardLin1 <RichardLin@my.unt.edu>
2019-04-15 12:07:12 -0500

    Remove font-variant-ligatures because there is no common system font that supports ligatures

--- docs/scripts/TestCSSFonts.js
+++ docs/scripts/TestCSSFonts.js
@@ -1,7 +1,7 @@
 function TestCSSFonts() {
     var test = new BrowserTest();
     test.title = "Fonts";
-    var properties = ['font-family', 'font-weight', 'font-stretch', 'font-style', 'font-size', 'font-size-adjust', 'font', 'font-synthesis', 'font-kerning', 'font-variant-ligatures', 'font-variant-position', 'font-variant-caps', 'font-variant-numeric', 'font-variant-east-asian', 'font-variant'];
+    var properties = ['font-family', 'font-weight', 'font-stretch', 'font-style', 'font-size', 'font-size-adjust', 'font', 'font-synthesis', 'font-kerning', 'font-variant-position', 'font-variant-caps', 'font-variant-numeric', 'font-variant-east-asian', 'font-variant'];
     for (var i = 0; i < properties.length; i++) {
         test.testDescriptions.push(properties[i]);
         test.results.push(Modernizr.testProp(properties[i]));
