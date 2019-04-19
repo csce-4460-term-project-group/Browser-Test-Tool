94b67bade50d9b5b01540415a27765e868f77df1
RichardLin1 <RichardLin@my.unt.edu>
2019-04-19 14:51:09 -0500

    Use Promise instead of EventListener

--- docs/scripts/TestHTML5AudioImplementation.js
+++ docs/scripts/TestHTML5AudioImplementation.js
@@ -1,10 +1,9 @@
-var totalTestsHTML5Audio = 3, testsCompletedHTML5Audio = 0;
-
 function TestHTML5AudioImplementation() {
     var test = new BrowserTest();
     test.title = "Audio";
     var formats = ["MP3", "Ogg", "WAV"];
     var files = ["audio.mp3", "audio.ogg", "audio.wav"];
+    var promises = [];
     for (var i = 0; i < formats.length; i++) {
         test.testDescriptions.push(formats[i]);
         var audio = new Audio("https://csce-4460-term-project-group.github.io/Browser-Test-Tool/test-audio/" + files[i]);
@@ -14,23 +13,13 @@ function TestHTML5AudioImplementation() {
             promise.then(function () {
                 parameters["test"].testDescriptionsUnsorted.push(parameters["format"]);
                 parameters["test"].results.push(true);
-                testsCompletedHTML5Audio++;
-                if (testsCompletedHTML5Audio == totalTestsHTML5Audio) {
-                    parameters["test"].sort();
-                    if (testsCompletedHTML5InputTypes + testsCompletedHTML5Video + testsCompletedHTML5Audio == totalTestsHTML5InputTypes + totalTestsHTML5Video + totalTestsHTML5Audio)
-                        document.dispatchEvent(new Event("html-5-implementation-tests-complete"));
-                }
             }).catch(function () {
                 parameters["test"].testDescriptionsUnsorted.push(parameters["format"]);
                 parameters["test"].results.push(false);
-                testsCompletedHTML5Audio++;
-                if (testsCompletedHTML5Audio == totalTestsHTML5Audio) {
-                    parameters["test"].sort();
-                    if (testsCompletedHTML5InputTypes + testsCompletedHTML5Video + testsCompletedHTML5Audio == totalTestsHTML5InputTypes + totalTestsHTML5Video + totalTestsHTML5Audio)
-                        document.dispatchEvent(new Event("html-5-implementation-tests-complete"));
-                }
             });
+            promises.push(promise);
         }({ test: test, audio: audio, format: formats[i] }));
     }
+    test.promise = Promise.all(promises);
     return test;
 }
--- docs/scripts/TestHTML5VideoImplementation.js
+++ docs/scripts/TestHTML5VideoImplementation.js
@@ -1,10 +1,9 @@
-var totalTestsHTML5Video = 5, testsCompletedHTML5Video = 0;
-
 function TestHTML5VideoImplementation() {
     var test = new BrowserTest();
     test.title = "Video";
     var formats = ["MP4 (AVC/H.264)", "MP4 (HEVC/H.265)", "Ogg (Theora)", "WebM (VP8)", "WebM (VP9)"];
     var files = ["video-h264.mp4", "video-h265.mp4", "video-theora.ogv", "video-vp8.webm", "video-vp9.webm"];
+    var promises = [];
     for (var i = 0; i < formats.length; i++) {
         test.testDescriptions.push(formats[i]);
         var video = document.createElement("video");
@@ -15,23 +14,13 @@ function TestHTML5VideoImplementation() {
             promise.then(function () {
                 parameters["test"].testDescriptionsUnsorted.push(parameters["format"]);
                 parameters["test"].results.push(true);
-                testsCompletedHTML5Video++;
-                if (testsCompletedHTML5Video == totalTestsHTML5Video) {
-                    parameters["test"].sort();
-                    if (testsCompletedHTML5InputTypes + testsCompletedHTML5Video + testsCompletedHTML5Audio == totalTestsHTML5InputTypes + totalTestsHTML5Video + totalTestsHTML5Audio)
-                        document.dispatchEvent(new Event("html-5-implementation-tests-complete"));
-                }
             }).catch(function () {
                 parameters["test"].testDescriptionsUnsorted.push(parameters["format"]);
                 parameters["test"].results.push(false);
-                testsCompletedHTML5Video++;
-                if (testsCompletedHTML5Video == totalTestsHTML5Video) {
-                    parameters["test"].sort();
-                    if (testsCompletedHTML5InputTypes + testsCompletedHTML5Video + testsCompletedHTML5Audio == totalTestsHTML5InputTypes + totalTestsHTML5Video + totalTestsHTML5Audio)
-                        document.dispatchEvent(new Event("html-5-implementation-tests-complete"));
-                }
             });
+            promises.push(promise);
         }({ test: test, video: video, format: formats[i] }));
     }
+    test.promise = Promise.all(promises);
     return test;
 }
