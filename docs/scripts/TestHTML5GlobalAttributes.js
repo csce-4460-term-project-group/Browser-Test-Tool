331303450eafa8f511285a9e93ba1f07a3a4b97b
RichardLin1 <RichardLin@my.unt.edu>
2019-04-20 16:16:53 -0500

    Use lowercase for descriptions

--- docs/scripts/TestHTML5GlobalAttributes.js
+++ docs/scripts/TestHTML5GlobalAttributes.js
@@ -1,11 +1,11 @@
 function TestHTML5GlobalAttributes() {
     var test = new BrowserTest();
     test.title = "Global Attributes";
+    test.testDescriptions = ["accesskey", "class", "contenteditable", "dir", "draggable", "hidden", "id", "lang", "spellcheck", "style", "tabindex", "title", "translate"];
     var arrayOfAttributes = ["accessKey", "className", "contentEditable", "dir", "draggable", "hidden", "id", "lang", "spellcheck", "style", "tabIndex", "title", "translate"];
     var myElem = document.createElement("a");
 
     for (var i = 0; i < arrayOfAttributes.length; i++) {
-        test.testDescriptions.push("<" + arrayOfAttributes[i] + ">");
         test.results.push((arrayOfAttributes[i]) in myElem);
     }
     return test;
