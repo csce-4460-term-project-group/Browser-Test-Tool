4b6dc2bbfa15f19b7d34d6afa5dd2755c8b59527
RichardLin1 <RichardLin@my.unt.edu>
2019-04-21 17:50:02 -0500

    Use HTMLElement.prototype instead of anchor element

--- docs/scripts/TestHTML5GlobalAttributes.js
+++ docs/scripts/TestHTML5GlobalAttributes.js
@@ -3,10 +3,9 @@ function TestHTML5GlobalAttributes() {
     test.title = "Global Attributes";
     test.testDescriptions = ["accesskey", "class", "contenteditable", "dir", "draggable", "hidden", "id", "lang", "spellcheck", "style", "tabindex", "title", "translate"];
     var arrayOfAttributes = ["accessKey", "className", "contentEditable", "dir", "draggable", "hidden", "id", "lang", "spellcheck", "style", "tabIndex", "title", "translate"];
-    var myElem = document.createElement("a");
 
     for (var i = 0; i < arrayOfAttributes.length; i++) {
-        test.results.push((arrayOfAttributes[i]) in myElem);
+        test.results.push((arrayOfAttributes[i]) in HTMLElement.prototype);
     }
     return test;
 }
